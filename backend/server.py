37be04c0f85a0970de2fc30b25570b465bd9a421
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2026-07-28 20:52:57 -0700

    Added endpoint to generate a lexigraph when given a word

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, send_file
+import io
 
 import corpus
+import lexigraphy
 
 app = Flask(__name__)
 
@@ -16,11 +18,24 @@ def get_corpus_count():
 def get_random_word():
     '''
     returns:
-    - image
     - manchu text
-    - font used
+    - romanized text
     '''
     return corpus.get_random_word()
+
+@app.route("/lexigraphy/new/<manchu>")
+def create_random_lexigraph(manchu):
+    '''
+    given:
+    - manchu text
+    returns:
+    - image of that text, generated with a random font
+    '''
+    lexigraph = lexigraphy.create_lexigraph(manchu)
+    image_io = io.BytesIO()
+    lexigraph.save(image_io, 'PNG')
+    image_io.seek(0)
+    return send_file(image_io, mimetype="image/png")
 
 @app.route("/")
 def hello_world():
