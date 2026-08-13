e8fc4d988346fd7c840c53c67012bb1ece9bf06a
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2026-08-13 06:29:19 -0700

    Added second layer to primary OCR

--- backend/primary_ocr.py
+++ backend/primary_ocr.py
@@ -17,23 +17,30 @@ class NeuralNetwork:
     NN_FILE_PATH = "saved_ann.json"
     
     def __init__(self, hidden_layer_size=64):
-        self.weight_x_h = randn(hidden_layer_size, constants.INPUT_LAYER_SIZE) / 1000
-        self.weight_h_y = randn(constants.OUTPUT_LAYER_SIZE, hidden_layer_size) / 1000
+        self.weight_x_h1 = randn(hidden_layer_size, constants.INPUT_LAYER_SIZE) / 1000
+        self.weight_h1_h2 = randn(hidden_layer_size, hidden_layer_size) / 1000
+        self.weight_h2_y = randn(constants.OUTPUT_LAYER_SIZE, hidden_layer_size) / 1000
 
-        self.bias_h = np.zeros((hidden_layer_size, 1))
+        self.bias_h1 = np.zeros((hidden_layer_size, 1))
+        self.bias_h2 = np.zeros((hidden_layer_size, 1))
         self.bias_y = np.zeros((constants.OUTPUT_LAYER_SIZE, 1))
 
     def get_neurons(self):
-        return { "weight_x_h": self.weight_x_h,
-                 "weight_h_y": self.weight_h_y,
-                 "bias_h": self.bias_h,
+        return { "weight_x_h": self.weight_x_h1,
+                 "weight_h_y": self.weight_h1_h2,
+                 "weight_h_y": self.weight_h2_y,
+                 "bias_h": self.bias_h1,
+                 "bias_h": self.bias_h2,
                  "bias_y": self.bias_y }
 
     def forward(self, x):
-        h = np.tanh(self.weight_x_h @ x + self.bias_h)
-        self.last_h = h
+        h1 = np.tanh(self.weight_x_h1 @ x + self.bias_h1)
+        self.last_h1 = h1
 
-        y = self.weight_h_y @ h + self.bias_y
+        h2 = np.tanh(self.weight_h1_h2 @ h1 + self.bias_h2)
+        self.last_h2 = h2
+
+        y = self.weight_h2_y @ h2 + self.bias_y
         self.last_y = y
 
         return softmax(y)
@@ -54,41 +61,56 @@ class NeuralNetwork:
 
         # Output Layer
 
-        d_t_d_w_h_y = self.last_h
+        d_t_d_w_h2_y = self.last_h2
         d_t_d_b_y = 1
-        d_t_d_h = self.weight_h_y
+        d_t_d_h = self.weight_h2_y
 
         d_L_d_t = gradient * d_o_d_t
 
-        d_L_d_w_h_y = d_L_d_t @ d_t_d_w_h_y.T
+        d_L_d_w_h2_y = d_L_d_t @ d_t_d_w_h2_y.T
         d_L_d_b_y = d_L_d_t * d_t_d_b_y
-        d_L_d_h = d_t_d_h.T @ d_L_d_t
+        d_L_d_h2 = d_t_d_h.T @ d_L_d_t
 
-        self.weight_h_y -= self.LEARNING_RATE * d_L_d_w_h_y
+        self.weight_h2_y -= self.LEARNING_RATE * d_L_d_w_h2_y
         self.bias_y -= self.LEARNING_RATE * d_L_d_b_y
 
-        # Hidden Layer
+        # Hidden Layer 2
 
-        d_t_d_w_x_h = input
-        d_t_d_b_h = 1
-        d_h_d_x = self.weight_x_h
+        d_t_d_w_h1_h2 = self.last_h1
+        d_t_d_b_h2 = 1
+        d_h2_d_h1 = self.weight_h1_h2
 
-        d_L_d_w_x_h = d_L_d_h @ d_t_d_w_x_h.T
-        d_L_d_b_h = d_L_d_h * d_t_d_b_h
-        d_L_d_x = d_h_d_x.T @ d_L_d_h
+        d_L_d_w_h1_h2 = d_L_d_h2 @ d_t_d_w_h1_h2.T
+        d_L_d_b_h2 = d_L_d_h2 * d_t_d_b_h2
+        d_L_d_h1 = d_h2_d_h1.T @ d_L_d_h2
 
-        self.weight_x_h -= self.LEARNING_RATE * d_L_d_w_x_h
-        self.bias_h -= self.LEARNING_RATE * d_L_d_b_h
+        self.weight_h1_h2 -= self.LEARNING_RATE * d_L_d_w_h1_h2
+        self.bias_h2 -= self.LEARNING_RATE * d_L_d_b_h2
+
+        # Hidden Layer 1
+
+        d_t_d_w_x_h1 = input
+        d_t_d_b_h1 = 1
+        d_h1_d_x = self.weight_x_h1
+
+        d_L_d_w_x_h1 = d_L_d_h1 @ d_t_d_w_x_h1.T
+        d_L_d_b_h1 = d_L_d_h1 * d_t_d_b_h1
+        d_L_d_x = d_h1_d_x.T @ d_L_d_h1
+
+        self.weight_x_h1 -= self.LEARNING_RATE * d_L_d_w_x_h1
+        self.bias_h1 -= self.LEARNING_RATE * d_L_d_b_h1
 
         return
 
     def save(self, filename):
         saved_values = {}
 
-        saved_values['weight_x_h'] = self.weight_x_h.tolist()
-        saved_values['weight_h_y'] = self.weight_h_y.tolist()
+        saved_values['weight_x_h1'] = self.weight_x_h1.tolist()
+        saved_values['weight_h1_h2'] = self.weight_h1_h2.tolist()
+        saved_values['weight_h_y'] = self.weight_h2_y.tolist()
 
-        saved_values['bias_h'] = self.bias_h.tolist()
+        saved_values['bias_h1'] = self.bias_h1.tolist()
+        saved_values['bias_h2'] = self.bias_h2.tolist()
         saved_values['bias_y'] = self.bias_y.tolist()
 
         with open(filename, 'w') as file:
