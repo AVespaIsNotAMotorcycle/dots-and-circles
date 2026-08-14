7d8d001fa358255619ba746f109001ffe332bbd3
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2026-08-13 20:44:34 -0700

    Setting up for heroku deployment

--- backend/server.py
+++ backend/server.py
@@ -242,3 +242,6 @@ def train_100_times():
 def hello_world():
     return "<p>Hello, World!</p>"
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
