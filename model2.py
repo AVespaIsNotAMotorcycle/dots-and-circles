6be54f1c1fd1f182f8abbb3a0e927da2d78f4422
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2026-08-24 16:36:51 -0700

    Adding backprop to model2

--- model2.py
+++ model2.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+from Levenshtein import ratio
 
 from utils import to_abkai, to_norman
 from lexigraphy import preprocess_image
@@ -35,13 +36,51 @@ class Model2():
 
         x = np.array(image).reshape((self.size_in, 1))
         y = self.ann.forward(x)
+        y = y.reshape(35, (len(ALPHABET)))
+        return y, x
+
+    def softmax(self, y):
+        for index, row in enumerate(y):
+            y[index] = np.exp(row) / sum(np.exp(row))
         return y
 
+    def cross_entropy_loss(self, probs, labels):
+        loss = 0
+        for index, row in enumerate(probs):
+            loss += -np.log(row[labels[index]])
+        return loss
+
+    def label_string_to_array(self, label):
+        labels = []
+        for char in label:
+            index = ALPHABET.index(char)
+            labels.append(index)
+        while len(labels) < 35:
+            labels.append(ALPHABET.index(' '))
+        return labels
+
     def train(self, batch):
+        successes = 0
+        loss = 0
         for image, label in batch:
-            print(label)
-            self.predict(image)
-        return 0, 0
+            y, x = self.predict(image)
+            probs = self.softmax(y)
+
+            predictions = np.argmax(y, axis=1)
+            word = ''
+            for char in predictions:
+                word += ALPHABET[char]
+            successes += ratio(word, label)
+
+            labels = self.label_string_to_array(label)
+            loss += self.cross_entropy_loss(probs, labels)
+
+            gradient = np.zeros(self.size_out)
+            for i, j in enumerate(labels):
+                gradient[(i * len(ALPHABET)) + j] = -1 / probs[i][j]
+            gradient = gradient.reshape((self.size_out, 1))
+            self.ann.backprop(x, y.reshape((self.size_out, 1)), gradient, 0.001)
+        return successes / len(batch), loss
 
     def save(self):
         return
