fc0af0e0185067b49f6c69bcce4f29e16f66a53d
sasha <sasha.m.ebersole@gmail.com>
2026-09-07 08:31:13 -0700

    Added checks to ensure weights and bias are of proper size

--- nn/layer/linear.py
+++ nn/layer/linear.py
@@ -7,12 +7,21 @@ class Linear(Layer):
                  weights=None, bias=None, use_bias=True):
         super().__init__(size_in, size_out)
 
-        self.weights = weights \
-            if weights is not None \
-            else np.random.rand(size_in, size_out)
-        self.bias = bias \
-            if bias is not None \
-            else np.zeros((1, size_out))
+        if weights is not None:
+            assert np.shape(weights) == (size_in, size_out), \
+                f"Expected weights to be of shape {(size_in, size_out)}," \
+                f"but it was {np.shape(weights)}"
+            self.weights = weights
+        else:
+            self.weights = np.random.rand(size_in, size_out)
+
+        if bias is not None:
+            assert np.shape(bias) == (1, size_out), \
+                f"Expected bias to be of shape {(1, size_out)}," \
+                f"but it was {np.shape(bias)}"
+            self.bias = bias
+        else:
+            self.bias = np.zeros((1, size_out))
 
     def forward(self, x):
         y = x @ self.weights
--- nn/layer/test_linear.py
+++ nn/layer/test_linear.py
@@ -3,12 +3,29 @@ import numpy as np
 from linear import Linear
 
 class TestLinear:
-    def test_sizes(self):
-        size_in = 5
-        size_out = 9
-        layer = Linear(size_in, size_out)
+    sizes = [(5, 9),
+             (1, 1),
+             (1, 5),
+             (5, 1),
+             (2, 2),
+             (2, 3),
+             (3, 2),
+             (1500, 87),
+             (3200, 3200)]
 
-        x = np.zeros((1, size_in))
-        y = layer(x)
-        expected = np.zeros((1, size_out))
-        assert np.array_equal(y, expected)
+    def test_sizes(self):
+        for size_in, size_out in self.sizes:
+            layer = Linear(size_in, size_out)
+            x = np.zeros((1, size_in))
+            y = layer(x)
+            expected = np.zeros((1, size_out))
+            assert np.shape(y) == np.shape(expected)
+            assert np.array_equal(y, expected)
+
+            weights = np.ones((size_in, size_out)) * 2
+            layer = Linear(size_in, size_out, weights=weights)
+            x = np.ones((1, size_in))
+            y = layer(x)
+            expected = np.ones((1, size_out)) * 2 * size_in
+            assert np.shape(y) == np.shape(expected)
+            assert np.array_equal(y, expected)
