Improved GELU.backward implementation - ensured correct shape and prevented cosh overflow

nn/GELU/__init__.py
@@ -14,12 +14,27 @@ class GELU(Module):
         y = a * b * c
         return y
 
+    def cosh(self, x):
+        # np.cosh will overflow if values are too high
+        cutoff = 100
+        x = np.clip(x, a_min=cutoff*-1, a_max=cutoff)
+        return np.cosh(x)
+
     def backward(self, dLdy):
         a = 0.5 * np.tanh((0.0356774 * self.last_x**3) \
             + (0.797885 * self.last_x))
         b = ((0.0535161 * self.last_x**3) + (0.398942 * self.last_x)) \
-            / np.cosh((0.0356774 * self.last_x**3) + (0.797885 * self.last_x))**2
+            / self.cosh((0.0356774 * self.last_x**3) + (0.797885 * self.last_x))**2
         c = 0.5
         dydx = a + b + c
         dLdx = dydx * dLdy
+        while sum(np.shape(dLdx)) > np.shape(self.last_x)[-1]:
+            dLdx = np.mean(dLdx, axis=0)
+        dLdx = dLdx.reshape(1, np.shape(self.last_x)[-1])
+
+        assert np.shape(dLdx) == (1, np.shape(self.last_x)[-1]), \
+            f"GELU expects dLdx to have the (1, z), where z is the final" \
+            f"dimension of the last x, "\
+            f"(z = {np.shape(self.last_x)[-1]}), but it was {np.shape(dLdx)}."
+        
         return dLdx