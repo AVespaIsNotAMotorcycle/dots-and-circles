70e57fcc0de9e4aadac965d73030cd3aaedfb9fb
sasha <sasha.m.ebersole@gmail.com>
2026-09-15 19:49:38 -0700

    Improved backpropogation - MultiHeadAttention still needs some work

--- nn/MultiHeadAttention/__init__.py
+++ nn/MultiHeadAttention/__init__.py
@@ -48,6 +48,7 @@ class MultiHeadAttention(Module):
         attn_scores = attn_scores + mask
 
         attn_weights = softmax(attn_scores / keys.shape[-1]**0.5)
+        self.last_softmax = attn_weights
         attn_weights = self.dropout(attn_weights)
 
         context_vec = (attn_weights @ values).transpose(0, 2, 1, 3)
@@ -59,17 +60,44 @@ class MultiHeadAttention(Module):
         return context_vec
 
     def backward(self, dLdy):
-        print("MultiHeadAttention.backward is a placeholder - redo it soon!")
-        dLdw = self.params['out_proj'].backward(dLdy)
+        assert np.shape(dLdy) == (1, self.size_out)
+        d_L_d_outproj = self.params['out_proj'].backward(dLdy)
         self.grads['out_proj'] = self.params['out_proj'].gradients()
 
-        dLdx = self.params['W_value'].backward(dLdw)
+        values_x = self.params['W_value'].last_y
+        keys_x = self.params['W_key'].last_y
+        queries_x = self.params['W_query'].last_y
+
+        weights_value = self.params['W_value'].parameters()['weights']
+        weights_value = np.mean(weights_value, axis=0).reshape(1, self.size_in)
+
+        weights_key = self.params['W_key'].parameters()['weights']
+        weights_key = np.mean(weights_key, axis=0).reshape(1, self.size_in)
+
+        weights_query = self.params['W_query'].parameters()['weights']
+        weights_query = np.mean(weights_query, axis=0).reshape(1, self.size_in)
+
+        last_softmax = self.last_softmax.T
+        if np.shape(last_softmax)[-1] > 1:
+            last_softmax = np.mean(last_softmax, axis=-1, keepdims=True)
+
+        dLdx = dLdy * ((values_x * ((-1 / queries_x * keys_x) * ((weights_query * keys_x) + (queries_x * weights_key)))) + (last_softmax * weights_value))
+
+        d_L_d_value = dLdy * last_softmax
+        self.params['W_value'].backward(d_L_d_outproj)
         self.grads['W_value'] = self.params['W_value'].gradients()
 
-        self.params['W_key'].backward(dLdw)
+        d_L_d_key = d_L_d_outproj * weights_query
+        self.params['W_key'].backward(d_L_d_key)
         self.grads['W_key'] = self.params['W_key'].gradients()
 
-        self.params['W_query'].backward(dLdw)
+        d_L_d_query = d_L_d_outproj * weights_key
+        self.params['W_query'].backward(d_L_d_query)
         self.grads['W_query'] = self.params['W_query'].gradients()
+
+        while sum(np.shape(dLdx)) > self.size_in:
+            dLdx = np.mean(dLdx, axis=0)
+        dLdx = dLdx.reshape(1, self.size_in)
+        assert np.shape(dLdx) == (1, self.size_in)
 
         return dLdx
--- nn/tests/test_Transformer.py
+++ nn/tests/test_Transformer.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from utils.softmax import softmax
 from nn.Transformer import Transformer
 
 def print_grad(grad, indent=0):
@@ -22,33 +23,56 @@ def compute_delta(grads, learn_rate):
     return delta
 
 class TestTransformer():
-    def test_SGD(self):
-        batch_size = 1
-        context_length = 1
-        emb_dim = 8
-
+    def run_test_SGD(self, batch_size, context_length, emb_dim):
+        print('\n', batch_size, context_length, emb_dim)
+        print("Total input embeddings:", batch_size * context_length * emb_dim)
+        
         np.random.seed(123)
         x = np.random.rand(batch_size, context_length, emb_dim)
         y = np.random.rand(batch_size, context_length, emb_dim)
+        y = np.argmax(y, axis=-1)
 
         transformer = Transformer(emb_dim, context_length, num_heads=4)
 
         loss_history = []
-        for _ in range(10):
+        iterations = 10
+        for _ in range(iterations):
             out = transformer(x)
+            probs = softmax(out)
             
-            mse_loss = np.sum((y - out)**2, keepdims=True) / len(out)
-            dLdout = -2 * (1 - out)
-            
-            transformer.backward(dLdout)
+            p_c = np.zeros((batch_size, context_length))
+            gradient = np.zeros((batch_size, context_length, emb_dim))
+            for item in range(batch_size):
+                for token in range(context_length):
+                    index = np.argmax(probs[item][token])
+                    prob = probs[item][token][index]
+                    p_c[item][token] = prob
+                    gradient[item][token][index] = -1 / prob
+            cross_entropy_loss = np.log(p_c) * -1
+            cross_entropy_loss = np.mean(cross_entropy_loss)
+            gradient = np.mean(gradient, axis=0)
+            gradient = np.mean(gradient, axis=0)
+            gradient = gradient.reshape(1, emb_dim)
+
+            transformer.backward(gradient)
             grads = transformer.gradients()
 
-            learn_rate = 0.01
+            learn_rate = 0.001
             delta = compute_delta(grads, learn_rate)
             transformer.descend(delta)
 
-            if len(loss_history) > 0:
-                assert mse_loss < loss_history[-1]
-            loss_history.append(mse_loss)
-        for entry in loss_history:
-            print(entry)
+            loss_history.append(cross_entropy_loss)
+        if np.isnan(loss_history[-1]): print("LOSS NAN")
+        if loss_history[0] > loss_history[-1]: print("Loss decreased")
+        if loss_history[0] < loss_history[-1]: print("Loss INCREASED")
+        return loss_history[0] > loss_history[-1]
+
+    def test_SGD(self):
+        loss_decreased = []
+        for batch_size in [1, 2, 4, 32]:
+            for context_length in [1, 4, 16, 64]:
+                for emb_dim in [4, 24, 32]:
+                    loss_decreased.append(
+                        self.run_test_SGD(batch_size, context_length, emb_dim)
+                    )
+        assert loss_decreased.count(True) > len(loss_decreased) * .67
